95677ab296c912dd28457c1704ac5df93501acec
Shane Song <xShane.Song@live.cn>
2025-01-21 22:15:12 +0800

    add code.

--- app/src/main/kotlin/com/example/geoquiz/MainActivity.kt
+++ app/src/main/kotlin/com/example/geoquiz/MainActivity.kt
@@ -12,6 +12,19 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +35,36 @@ class MainActivity : ComponentActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener { view: View ->
-            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
-            toast.setGravity(Gravity.START, 100, 100)
-            toast.show()
+//            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+//            toast.setGravity(Gravity.START, 100, 100)
+//            toast.show()
+            checkAnswer(true)
         }
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            checkAnswer(false)
         }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
 }
