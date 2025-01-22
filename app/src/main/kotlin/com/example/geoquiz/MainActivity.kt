09ba99fa5773f4b5c95ba4ce241cd491acb6ba08
SONG MING <songm@neusoft.com>
2025-01-22 09:02:46 +0800

    use image button.

--- app/src/main/kotlin/com/example/geoquiz/MainActivity.kt
+++ app/src/main/kotlin/com/example/geoquiz/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.geoquiz
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -12,8 +13,10 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var previousButton: Button
-    private lateinit var nextButton: Button
+//    private lateinit var previousButton: Button
+//    private lateinit var nextButton: Button
+    private lateinit var previousButton: ImageButton
+    private lateinit var nextButton: ImageButton
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
