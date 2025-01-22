942e859152776e2626493a8f8a622e54ace04ce8
SONG MING <songm@neusoft.com>
2025-01-22 08:02:22 +0800

    add image to resource.

--- app/src/main/kotlin/com/example/geoquiz/MainActivity.kt
+++ app/src/main/kotlin/com/example/geoquiz/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.geoquiz
 
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +12,7 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var previousButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
 
@@ -33,6 +34,9 @@ class MainActivity : ComponentActivity() {
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        previousButton = findViewById(R.id.previous_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
 //            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
@@ -43,6 +47,14 @@ class MainActivity : ComponentActivity() {
         falseButton.setOnClickListener { view: View ->
 //            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
             checkAnswer(false)
+        }
+
+        previousButton.setOnClickListener {
+            currentIndex -= 1
+            if (currentIndex < 0) {
+                currentIndex = questionBank.size - 1
+            }
+            updateQuestion()
         }
 
         nextButton.setOnClickListener {
