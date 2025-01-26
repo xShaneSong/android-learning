5c1c958efbf5d71ee697a1076f1fbd18e8a649ff
SONG MING <songm@neusoft.com>
2025-01-26 11:20:49 +0800

    add new project.

---
+++ app/src/main/kotlin/com/example/criminalintent/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.criminalintent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}
