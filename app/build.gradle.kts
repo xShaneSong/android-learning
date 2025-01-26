3518934c1664ff6c5ea7e6fad19550740b9168a1
SONG MING <songm@neusoft.com>
2025-01-26 14:48:31 +0800

    add recyclerview.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.recyclerview)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
